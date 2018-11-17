2335d8c0093d96b04eaf9127ea1deb2862c886f2
Soo J. Cho <beantownbuilder@gmail.com>
2018-11-16 21:04:05 -0500

    connect/disconnect from public server log

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+
 //create routes, middleware, startup the server
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 //configure express middleware to serve up the public folder
 app.use(express.static(publicPath));
